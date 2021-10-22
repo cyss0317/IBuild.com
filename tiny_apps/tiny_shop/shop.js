ebeccbb10de1f85e06244706d78a1d3fa534bcb8
Yun Sung Choi <cyss0317@gmail.com>
2021-10-21 22:56:08 -0500

    tiny shop added

---
+++ tiny_apps/tiny_shop/shop.js
@@ -0,0 +1,25 @@
+//Layout === All other apps will
+// DO NOT AMMEND INDEX.HTML BODY
+// Write all code in this file
+
+//DISPLAY
+//User lands on page => grid of items
+//grid item has an image, price, "buy button"
+//"buy button" => adds item to cart
+
+//OPTIONS
+// Cart button  innerHTML = Cart: $0.00
+// Cart Button updates total value of the cart;
+//when we click cart button -> replace display with items in the Cart
+//cart button => "Back to Shop"
+//checkout button
+
+//OUt of Scope
+//"Backend" persists and serves Shop items
+//NOT process cart, sign up users, etc.
+
+//Stack
+//HTML/CSS/JS => Test w local storage
+// production => DB(noSQL), backend (severless), Image Storage ( AWS S3)
+
+//Research: 
