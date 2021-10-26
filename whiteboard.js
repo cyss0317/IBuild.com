668c27e0de7a8b3d262d516afb83c2db1b8b77e1
Yun Sung Choi <cyss0317@gmail.com>
2021-10-26 15:58:59 -0500

    cupPhoto whiteboard

---
+++ whiteboard.js
@@ -0,0 +1,51 @@
+const devtimeCup = (matches, outcome) => {
+    let score = {};
+    let currentWinningPoint = 0;
+    let currentWinner ;
+    //iterate through outcome, and store the currentWinning team and compare the current point for the number 
+    for( let i = 0; i < outcome.length; i ++){
+        let winner = matches[i][outcome[i] - 1];
+
+        if(!score[winner]){
+            score[winner] = 1;
+            currentWinningPoint++;
+            if(!currentWinner){
+                currentWinner = winner;
+            }
+        } else {
+            score[winner]++;
+            if(score[winner] > currentWinningPoint){
+                currentWinningPoint = score[winner];
+                currentWinner = winner;
+            }
+        }
+    }
+    return currentWinner;
+
+} 
+
+const matches = [['germany', 'spain'], ['spain', "england"], ['england', 'germany']]
+const outcome = [2,2,1];
+
+return devtimeCup(matches, outcome);
+
+
+first = [0,2,6,5,2,3,1,3,3,9]
+// first = [1,8,7,3,1,3,4,2,3,1]
+const cupPhoto = (first, second) => {
+    //two separate iterations 
+        // iterate over the first row and find out if all condition is true; then return true
+    let isPossible = false;
+    first.sort((a,b) => a - b);
+    second.sort((a,b) => a - b);
+    for(let i = 0; i < first.length; i++){  
+        first[i] > second[i] ? isPossible = true: isPossible = false;
+    }
+    if( isPossible ) return true;
+    for (let i = 0; i < first.length; i++) {
+        first[i] < second[i] ? isPossible = true : isPossible = false;
+    }
+
+    return isPossble;
+
+}
